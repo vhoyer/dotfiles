f06ce64ad49590b1600fa77b14e3be82355cb1ae
Vinícius Hoyer <contact@vhoyer.dev>
2020-07-07 13:55:32 -0300

    :sparkles: [nvim] make shellscript template better

--- home-files/.config/nvim/templates/shellscript.sh
+++ home-files/.config/nvim/templates/shellscript.sh
@@ -1,1 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+PRGNAME="$(basename $0)"
+
+SHORTHELP="usage: /$PRGNAME (-[h]|--help)?/
+for a detailed list of options, use: \`$PRGNAME --help\`
+"
+
+HELPMSG="if you run $PRGNAME with no options it will display
+the commitizen categories.
+
+  -h\tdisplays this message
+"
+
+case "$1" in
+  # you can insert your own options here to do stuff
+  "--help")
+    printf "%s" "$HELPMSG"
+    ;;
+  "-h")
+    printf "%s"  "$SHORTHELP"
+    ;;
+  *)
+    printf "unknown option: '%s'\n\n%s" "$1" "$SHORTHELP"
+    exit 1
+    ;;
+esac
