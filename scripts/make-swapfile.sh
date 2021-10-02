ca9307a73b6139f49ee2c89828fe1e87946d7d9b
Vinícius Hoyer <contact@vhoyer.dev>
2021-10-01 23:11:21 -0300

    :sparkles: [make-swapfile] add ability to change swapfile location

--- scripts/make-swapfile.sh
+++ scripts/make-swapfile.sh
@@ -22,7 +22,14 @@ OPTIONS:
                      the <size> must be in the following format:
                      /\\d+[GM]/
   --deallocate       delete the swap
+  --file <path>      which file to use as swapfile (defaults to: '/swapfile')
 "
+
+SWAPFILE=/swapfile
+
+fstabContent() {
+  echo "$SWAPFILE swap swap defaults 0 0"
+}
 
 checkRoot() {
   if [ "$EUID" -ne 0 ]
@@ -32,54 +39,76 @@ checkRoot() {
 }
 
 allocate() {
+  if [ ! $SIZE ]; then
+    echo "No <size> was provided to allocate"
+    exit 1
+  fi
+
   # 1. create the file
-  fallocate -l $1 /swapfile
+  # fallocate -l $SIZE $SWAPFILE
   # if fail run:
-  # sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+  # dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+  BYTE_SIZE=`numfmt --from=iec $SIZE`
+  SECTOR_SIZE=1024
+  SECTOR_COUNT=`expr $BYTE_SIZE / $SECTOR_SIZE`
+  dd if=/dev/zero of=$SWAPFILE bs=$SECTOR_SIZE count=$SECTOR_COUNT status=progress
   #
   # 2. Set correct permissions
-  chmod 600 /swapfile
+  chmod 600 $SWAPFILE
   #
   # 3. Set up a linux swap area
-  mkswap /swapfile
+  mkswap $SWAPFILE --verbose
   #
   # 4. Enable the swap
-  swapon /swapfile
+  swapon $SWAPFILE
   #
   # 5. make it persistent
-  echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+  fstabContent >> /etc/fstab
 }
 
 deallocate() {
   # 1. First, deactivate the swap using the following command
-  swapoff -v /swapfile
+  swapoff -v $SWAPFILE
   #
   # 2. Remove the swap file entry /swapfile swap swap defaults 0 0 from the /etc/fstab file
-  sed -i -e "/^\/swapfile swap swap defaults 0 0$/d" /etc/fstab
+  local FSTAB_CONTENT=`fstabContent`
+  sed -i -e "/^${FSTAB_CONTENT//\//\\\/}$/d" /etc/fstab
   #
   # 3. Delete the actual swapfile
-  rm /swapfile
+  rm $SWAPFILE
 }
 
-case "$1" in
-  # you can insert your own options here to do stuff
-  "--help")
-    printf "%s" "$HELPMSG"
-    ;;
-  "-h")
-    printf "%s"  "$SHORTHELP"
-    ;;
-  "--allocate")
-    checkRoot
-    shift
-    allocate $1
-    ;;
-  "--deallocate")
-    checkRoot
-    deallocate
-    ;;
-  *)
-    printf "unknown option: '%s'\n\n%s" "$1" "$SHORTHELP"
-    exit 1
-    ;;
-esac
+showHelp() {
+  printf "%s" "$HELPMSG"
+}
+
+CMD=showHelp
+
+while [ -n "$1" ]; do
+  case "$1" in
+    # you can insert your own options here to do stuff
+    "--help"|"-h") showHelp ;;
+    "--file")
+      shift
+      SWAPFILE="$1"
+      ;;
+    "--allocate")
+      checkRoot
+      CMD=allocate
+      shift
+      SIZE=$1
+      ;;
+    "--deallocate")
+      checkRoot
+      CMD=deallocate
+      ;;
+    *)
+      printf "unknown option: '%s'\n\n%s" "$1" "$SHORTHELP"
+      exit 1
+      ;;
+  esac
+
+  shift
+done
+
+$CMD
