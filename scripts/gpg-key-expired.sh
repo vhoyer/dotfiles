7a32e4e5a842c27c7cbc73f2fc1506f77f2e5fa5
Vinícius Hoyer <contact@vhoyer.dev>
2024-01-02 16:17:43 -0300

    :memo: [gpg] add script to help regenerate the gpg key for git and stuff

---
+++ scripts/gpg-key-expired.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# https://gist.github.com/TheSherlockHomie/a91d3ecdce8d0ea2bfa38b67c0355d00
+
+echo "I'm gonna curse myself very much in one year, but just do it yourself, up in there is the link"
