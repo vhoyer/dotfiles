9536e542a54100585c707879e1ae706feadfb38c
Vinícius Hoyer <contact@vhoyer.dev>
2024-02-17 10:41:39 -0300

    :bug: This empty funciton is really needed after all

--- home-files/.config/nvim/plugins-config/hrsh7th__nvim-cmp.lua
+++ home-files/.config/nvim/plugins-config/hrsh7th__nvim-cmp.lua
@@ -5,15 +5,15 @@ cmp.setup({
 		completion = {
 			completeopt = 'menu,menuone,preview,noinsert',
 		},
-		-- snippet = {
-		-- 	-- REQUIRED - you must specify a snippet engine
-		-- 	expand = function(--[[ args ]])
-		-- 		-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-		-- 		-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-		-- 		-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-		-- 		-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-		-- 	end,
-		-- },
+		snippet = {
+			-- REQUIRED - you must specify a snippet engine
+			expand = function(--[[ args ]])
+				-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+				-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+				-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+				-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+			end,
+		},
 		window = {
 			-- completion = cmp.config.window.bordered(),
 			-- documentation = cmp.config.window.bordered(),
