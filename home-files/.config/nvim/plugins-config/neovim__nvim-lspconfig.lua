482122f4cf947e7d7cb19298896cf3d40f82cbe0
Vinícius Hoyer <contact@vhoyer.dev>
2024-06-04 16:53:41 -0300

    :bug: [nvim] configure volar hybrid without takeover

--- home-files/.config/nvim/plugins-config/neovim__nvim-lspconfig.lua
+++ home-files/.config/nvim/plugins-config/neovim__nvim-lspconfig.lua
@@ -27,12 +27,25 @@ lspconfig.stylelint_lsp.setup({
 	},
 })
 
+local mason_registry = require('mason-registry')
+local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
+
 lspconfig.tsserver.setup({
 	capabilities = capabilities,
+	init_options = {
+		plugins = {
+			{
+				name = '@vue/typescript-plugin',
+				location = vue_language_server_path,
+				languages = { 'vue' },
+			},
+		},
+	},
+	filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
 })
 
 lspconfig.volar.setup({
-	filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' }
+	capabilities = capabilities,
 })
 
 lspconfig.vimls.setup({
